3646fdcbbe528b7f1b9f9d74218d8ceace888df9
baburello <babur565@gmail.com>
2024-04-19 16:01:48 +0500

    feat: implement highscore fnctn

--- script.js
+++ script.js
@@ -7,8 +7,8 @@
 // console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,6 +23,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     //when the guess is too high
   } else if (guess > secretNumber) {
