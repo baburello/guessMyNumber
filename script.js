579c6bbfa9b43bc391710d31683775e0a64e0ae1
baburello <babur565@gmail.com>
2024-04-19 14:15:56 +0500

    :feat: add random num generrator function

--- script.js
+++ script.js
@@ -5,3 +5,15 @@
 // document.querySelector('.score').textContent = 20;
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
+
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number 🧐';
+  }
+});
